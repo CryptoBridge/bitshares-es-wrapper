c79b9c456b23a404c4078da3d203e204b1516afb
Crypto <crypto@crypto-bridge.org>
2018-03-07 13:35:45 +0100

    Add error handling

--- wrapper.js
+++ wrapper.js
@@ -13,6 +13,10 @@ server.get( '/get_account_history', (req, response, next) => {
     var sortby = req.query.sort_by;
     var size = req.query.size;
 
+    if (size > 10000) {
+        size = 10000;
+    }
+
     const json = {
         "sort": [{"block_data.block_time": {"order": "desc"}}],
         "query": {"bool": {"must": [{"match": {"account_history.account":account_id}},
@@ -20,14 +24,26 @@ server.get( '/get_account_history', (req, response, next) => {
         "from": 0, "size": size
     };
 
+
+
     request
       .post('http://localhost:9200/_search')
       .send(json)
       .set('accept', 'json')
       .end((err, res) => {
-          if (!err)
+
+          if (err) {
+              console.error(err);
+              return response.status(err.status || 500).json(err);
+          }
+
+          if (!res.body || !res.body.hits || !res.body.hits.hits ) {
+              console.warn('Empty result');
+              return response.json([]);
+          }
           if (res.body.hits.hits) {
                 var results = [];
+
                 res.body.hits.hits.map((item) => {
                     results.push(item._source);
                 });
