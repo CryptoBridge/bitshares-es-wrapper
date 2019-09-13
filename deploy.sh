9f91b7aec3e8aaa4dab9d53e128a5768f04ca779
jquijino <jay.quijino@gmail.com>
2019-09-13 11:16:06 +0200

    Improves deployment

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,17 @@
 # Login first as cryptobridge using
 # docker login
 
-docker build -t cryptobridge/bitshares-es-wrapper:latest .
-docker push cryptobridge/bitshares-es-wrapper:latest
+PACKAGE="cryptobridge/bitshares-es-wrapper"
+VERSION=$(grep version package.json | awk -F'"' '$0=$4')
+
+echo docker build --no-cache -t $PACKAGE:latest .
+echo docker push $PACKAGE:latest
+
+echo
+read -p "Also push version ${VERSION}? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo docker tag $PACKAGE:latest $PACKAGE:$VERSION
+    echo docker push $PACKAGE:$VERSION
+fi
